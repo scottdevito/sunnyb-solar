393a7d7af18c669b4e13caa36fa50e180159b5ed
Scott DeVito <scottadevito@gmail.com>
2018-03-24 09:48:20 -0400

    NavLink positioning

--- src/components/app_bar_nav.MUI.js
+++ src/components/app_bar_nav.MUI.js
@@ -22,43 +22,49 @@ function AppBarNav(props) {
           <Typography variant="title" color="inherit">
             Sunny B Solar
           </Typography>
-          <StyledNavLink
-            exact
-            to="/"
-            activeStyle={{
-              backgroundImage:
-                'linear-gradient(120deg, #F79522 0%, #FCD306 100%)',
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: '0 100%',
-              backgroundSize: '100% 0.2em',
-            }}
-          >
-            Home
-          </StyledNavLink>
-          <StyledNavLink
-            to="/about"
-            activeStyle={{
-              backgroundImage:
-                'linear-gradient(120deg, #F79522 0%, #FCD306 100%)',
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: '0 100%',
-              backgroundSize: '100% 0.2em',
-            }}
-          >
-            About Us
-          </StyledNavLink>
-          <StyledNavLink
-            to="/pricing"
-            activeStyle={{
-              backgroundImage:
-                'linear-gradient(120deg, #F79522 0%, #FCD306 100%)',
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: '0 100%',
-              backgroundSize: '100% 0.2em',
-            }}
-          >
-            Pricing Plans
-          </StyledNavLink>
+          <ToolbarRightGroup>
+            <StyledNavLink
+              exact
+              to="/"
+              activeStyle={{
+                backgroundImage:
+                  'linear-gradient(120deg, #F79522 0%, #FCD306 100%)',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: '0 100%',
+                backgroundSize: '100% 0.2em',
+              }}
+            >
+              Home
+            </StyledNavLink>
+
+            <StyledNavLink
+              to="/about"
+              activeStyle={{
+                backgroundImage:
+                  'linear-gradient(120deg, #F79522 0%, #FCD306 100%)',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: '0 100%',
+                backgroundSize: '100% 0.2em',
+              }}
+            >
+              About Us
+            </StyledNavLink>
+
+            <StyledNavLink
+              to="/pricing"
+              activeStyle={{
+                backgroundImage:
+                  'linear-gradient(120deg, #F79522 0%, #FCD306 100%)',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: '0 100%',
+                backgroundSize: '100% 0.2em',
+              }}
+            >
+              Pricing Plans
+            </StyledNavLink>
+
+            <StyledPhoneNum>(516) 123-4567</StyledPhoneNum>
+          </ToolbarRightGroup>
         </Toolbar>
       </AppBar>
     </div>
@@ -74,4 +80,28 @@ export default withStyles(styles)(AppBarNav);
 const StyledNavLink = styled(NavLink)`
   color: #000;
   text-decoration: none;
+  background-position: 0 100%;
+  background-size: 0% 0.2em;
+  margin-left: 1rem;
+  margin-right: 1rem;
+
+  &:hover {
+    background-image: linear-gradient(120deg, #fcd306 0%, #fcd306 100%);
+    background-repeat: no-repeat;
+    background-position: 0 100%;
+    background-size: 100% 0.2em;
+    transition: 1s ease all;
+  }
+`;
+
+const StyledPhoneNum = styled.p`
+  font-weight: bold;
+`;
+
+const ToolbarRightGroup = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
 `;
